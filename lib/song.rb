41c5a228aabe5c55387afa14b3aefb8baaa5115e
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-03 17:25:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,11 +6,7 @@ class Song
   end
 
   def artist_name
-    if self.artist != nil
-      self.artist.name
-    else
-      nil
-    end
+    if self.artist != nil {self.artist.name: nil}
   end
 
 end
