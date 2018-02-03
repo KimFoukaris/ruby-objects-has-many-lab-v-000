283549e173193324dbedc95a11ae7e293707dffc
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-03 17:19:39 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,25 @@
 class Author
   attr_accessor :name, :posts
+  @@post_count
 
   def initialize(name)
     @name = name
     @posts = []
+  end
+
+  def add_post(post)
+    self.posts << post
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.posts << post
+    post.author = self
+  end
+
+  def self.post_count
+
   end
 
 end
